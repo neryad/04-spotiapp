0692d7937d8bb6dac197220d932d1314b2355ca8
Neryad <dayern.gomez@hotmail.com>
2019-12-06 09:27:29 -0400

    app start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { ArtistaComponent } from './components/artista/artista.component';
+import { NavbaraComponent } from './components/shared/navbara/navbara.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    ArtistaComponent,
+    NavbaraComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/shared/navbara/navbara.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbara',
+  templateUrl: './navbara.component.html',
+  styles: []
+})
+export class NavbaraComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
