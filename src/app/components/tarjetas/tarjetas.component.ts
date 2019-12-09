5de8dfdcad2dcb515b78140eea736668b1cdfa37
Neryad <dayern.gomez@hotmail.com>
2019-12-08 21:41:33 -0400

    route to find artist

--- src/app/components/tarjetas/tarjetas.component.ts
+++ src/app/components/tarjetas/tarjetas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tarjetas',
@@ -8,7 +9,18 @@ import { Component, OnInit, Input } from '@angular/core';
 export class TarjetasComponent implements OnInit {
   @Input() items: any[] = [];
 
-  constructor() { }
+  constructor( private router: Router) { }
+
+  verArtista(item: any) {
+    let artistId;
+    if ( item.type === 'artist') {
+      artistId = item.id;
+    } else {
+      artistId = item.artists[0].id;
+    }
+    this.router.navigate([ '/artist', artistId ]);
+
+  }
 
   ngOnInit() {
   }
